bbbf4b5b662852095f854c5870c0f8861839594a
keung2020 <62239958+keung2020@users.noreply.github.com>
2023-11-30 07:10:30 +0800

    Update feed-clean.sh

--- feed-clean.sh
+++ feed-clean.sh
@@ -52,7 +52,7 @@ rm -rf feeds/packages/net/hysteria
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/shadowsocks-rust
 rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/frp
-svn co https://github.com/fatedier/frp/branches/master feeds/packages/net/frp
+#rm -rf feeds/packages/net/frp
+#svn co https://github.com/fatedier/frp/branches/master feeds/packages/net/frp
 #svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/net/frp feeds/packages/net/frp
 ./scripts/feeds update -a
