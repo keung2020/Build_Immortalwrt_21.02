6f75422804858840a7d7a7bd737c76bd09fea9f9
keung2020 <62239958+keung2020@users.noreply.github.com>
2024-03-06 06:58:53 +0800

    Update feed-cleanx86.sh

--- feed-cleanx86.sh
+++ feed-cleanx86.sh
@@ -52,35 +52,10 @@ rm -rf feeds/packages/net/hysteria
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/shadowsocks-rust
 rm -rf feeds/packages/net/mosdns
-
-function merge_package() {
-    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
-    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-    if [[ $# -lt 3 ]]; then
-        echo "Syntax error: [$#] [$*]" >&2
-        return 1
-    fi
-    trap 'rm -rf "$tmpdir"' EXIT
-    branch="$1" curl="$2" target_dir="$3" && shift 3
-    rootdir="$PWD"
-    localdir="$target_dir"
-    [ -d "$localdir" ] || mkdir -p "$localdir"
-    tmpdir="$(mktemp -d)" || exit 1
-    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-    cd "$tmpdir"
-    git sparse-checkout init --cone
-    git sparse-checkout set "$@"
-    # 使用循环逐个移动文件夹
-    for folder in "$@"; do
-        mv -f "$folder" "$rootdir/$localdir"
-    done
-    cd "$rootdir"
-}
-
-rm -rf feeds/packages/net/frp
-merge_package 23.05 https://github.com/immortalwrt/packages feeds/packages/net net/frp
+# rm -rf feeds/packages/net/frp
+# merge_package 23.05 https://github.com/immortalwrt/packages feeds/packages/net net/frp
 # svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/net/frp feeds/packages/net/frp
-rm -rf feeds/packages/lang/golang
-#git clone https://github.com/openwrt/packages/tree/openwrt-23.05/lang/golang feeds/packages/lang/golang
-merge_package 23.05 https://github.com/immortalwrt/packages feeds/packages/lang lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/openwrt/packages/tree/openwrt-23.05/lang/golang feeds/packages/lang/golang
+# merge_package 23.05 https://github.com/immortalwrt/packages feeds/packages/lang lang/golang
 ./scripts/feeds update -a
